000e89f12b2d615c425e18c9b932791d21ee1c8e
Lucas Meoli <meolilucas@gmail.com>
2025-03-24 10:52:46 -0300

    Modify comments

--- leds/inc/leds.h
+++ leds/inc/leds.h
@@ -1,5 +1,5 @@
 /************************************************************************************************
-Copyright (c) 2024, Lucas Meoli <meolilucas@gmail.com>
+Copyright (c) 2025, Lucas Meoli <meolilucas@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
